e40d1f099f59be0734588ccff15d461363a36bbc
steve <stevechap405@gmail.com>
2024-05-15 16:57:41 +0200

    Create getAllPost - getMyPost

--- back/src/Controllers/Routes/post.js
+++ back/src/Controllers/Routes/post.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createPost } = require("../postController");
+const { createPost, getAllPost, getMyPost } = require("../postController");
 const { verifyToken } = require("../../Utils/extractToken");
 
-const routerPost = express.Router();
+const router = express.Router();
 
-routerPost.route("/createPost").post(createPost);
+router.route("/createPost").post(createPost);
+router.route("/all").get(getAllPost);
+router.route("/myPost").get(getMyPost);
 
-module.exports = routerPost;
+module.exports = router;
--- back/src/Controllers/postController.js
+++ back/src/Controllers/postController.js
@@ -6,6 +6,7 @@ const { extractToken } = require("../Utils/extractToken");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+// Ajout d'un post (message)
 const createPost = async (request, response) => {
   const token = await extractToken(request);
 
@@ -17,22 +18,17 @@ const createPost = async (request, response) => {
         .json({ err: "Requête non autorisée le Token n'est pas bon" });
       return;
     } else {
-      if (
-        !request.body.title ||
-        !request.body.body ||
-        !request.body.user_id ||
-        !request.body.status
-      ) {
+      if (!request.body.title || !request.body.body) {
         response.status(400).json({ error: "Des champs sont manquants" });
       }
       try {
-        let post = new Post(
+        let post = new Posts(
           request.body.title,
           request.body.body,
-          request.body.user_id,
-          request.body.status,
+          authData.id,
+          "published",
           //   authData.id,
-          new Date()
+          new Date().toLocaleDateString("fr")
           //   "published"
         );
         let result = await client
@@ -48,6 +44,43 @@ const createPost = async (request, response) => {
   });
 };
 
+// Afficher tous les posts
+const getAllPost = async (request, response) => {
+  console.log("played");
+
+  let post = await client.db("mingle_sphere").collection("post").find();
+  let apiResponse = await post.toArray();
+  response.status(200).json(apiResponse);
+};
+
+// Afficher mes posts
+const getMyPost = async (request, response) => {
+  const token = await extractToken(request);
+
+  jwt.verify(token, process.env.SECRET_KEY, async (err, authData) => {
+    if (err) {
+      console.log(err);
+      response
+        .status(401)
+        .json({ err: "Requête non autorisée le Token n'est pas bon" });
+      return;
+    } else {
+      let post = await client
+        .db("mingle_sphere")
+        .collection("post")
+        .find({ user_id: authData.id });
+      let apiResponse = await post.toArray();
+      response.status(200).json(apiResponse);
+    }
+  });
+};
+
+// Supprimer un post
+
+// Modifier un post
+
 module.exports = {
   createPost,
+  getAllPost,
+  getMyPost,
 };
