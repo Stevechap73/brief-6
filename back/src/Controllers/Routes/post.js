dccb418ea3bd04b25dda62e747aa8065d7b0d657
steve <stevechap405@gmail.com>
2024-05-16 09:54:31 +0200

    update

--- back/src/Controllers/Routes/post.js
+++ back/src/Controllers/Routes/post.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { createPost, getAllPost, getMyPost } = require("../postController");
+const {
+  createPost,
+  getAllPost,
+  getMyPost,
+  deletePost,
+} = require("../postController");
 const { verifyToken } = require("../../Utils/extractToken");
 
 const router = express.Router();
 
 router.route("/createPost").post(createPost);
 router.route("/all").get(getAllPost);
-router.route("/myPost").get(getMyPost);
+router.route("/myPost", verifyToken).get(getMyPost);
+router.route("/delete", verifyToken).delete(deletePost);
 
 module.exports = router;
--- back/src/Controllers/postController.js
+++ back/src/Controllers/postController.js
@@ -17,29 +17,28 @@ const createPost = async (request, response) => {
         .status(401)
         .json({ err: "Requête non autorisée le Token n'est pas bon" });
       return;
-    } else {
-      if (!request.body.title || !request.body.body) {
-        response.status(400).json({ error: "Des champs sont manquants" });
-      }
-      try {
-        let post = new Posts(
-          request.body.title,
-          request.body.body,
-          authData.id,
-          "published",
-          //   authData.id,
-          new Date().toLocaleDateString("fr")
-          //   "published"
-        );
-        let result = await client
-          .db("mingle_sphere")
-          .collection("post")
-          .insertOne(post);
-        response.status(200).json(result);
-      } catch (e) {
-        console.log(e);
-        response.status(500).json(e);
-      }
+    }
+    if (!request.body.title || !request.body.body) {
+      response.status(400).json({ error: "Des champs sont manquants" });
+    }
+    try {
+      let post = new Posts(
+        request.body.title,
+        request.body.body,
+        authData.id,
+        "published",
+        //   authData.id,
+        new Date().toLocaleDateString("fr")
+        //   "published"
+      );
+      let result = await client
+        .db("mingle_sphere")
+        .collection("post")
+        .insertOne(post);
+      response.status(200).json(result);
+    } catch (e) {
+      console.log(e);
+      response.status(500).json(e);
     }
   });
 };
@@ -76,6 +75,47 @@ const getMyPost = async (request, response) => {
 };
 
 // Supprimer un post
+const deletePost = async (request, response) => {
+  if (!request.body.user_id) {
+    response.status(400).json({ error: "Le champs id est manquant" });
+    return;
+  }
+  let id = new ObjectId(request.body.id);
+
+  const token = await extractToken(request);
+
+  jwt.verify(token, process.env.SECRET_KEY, async (err, authData) => {
+    if (err) {
+      console.log(err);
+      response
+        .status(401)
+        .json({ err: "Requête non autorisée le Token n'est pas bon" });
+      return;
+    }
+
+    let post = await client
+      .db("mingle_sphere")
+      .collection("post")
+      .find({ user_id: authData.id });
+    if (!post) {
+      response.status(401).json({ error: "Requête non autorisée" });
+      return;
+    }
+    if (authData.id !== post.user_id || authData.role_id !== "1") {
+      response.status(401).json({ error: "Requête non autorisée" });
+      return;
+    }
+    try {
+      await client
+        .db("mingle_sphere")
+        .collection("post")
+        .deleteOne({ _id: id });
+    } catch (e) {
+      console.log(e);
+      response.status(500).json(e);
+    }
+  });
+};
 
 // Modifier un post
 
@@ -83,4 +123,5 @@ module.exports = {
   createPost,
   getAllPost,
   getMyPost,
+  deletePost,
 };
